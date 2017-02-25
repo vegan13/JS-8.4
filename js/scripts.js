ec2fa84501f92feaf49eecce175b5221961a9c9f
vegan13 <vegan13@o2.pl>
2017-02-25 17:57:14 +0100

    pole trojkata

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var a = 5,
+    h = 10,
+    triangleArea = 1;
+
+a = prompt('Wpisz długość podstawy trójkąta');
+h = prompt('Wpisz wysokość trójkąta');
+
+triangleArea = a * h / 2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
